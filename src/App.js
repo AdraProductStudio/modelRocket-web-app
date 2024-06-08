45d05ec4ebd0efa730842ba45d6f021af519f8b8
AdraProductStudio <adraforyou@gmail.com>
2024-06-08 23:54:37 +0530

    UPDATE : Integrate With get consumer api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Component/Home";
 import AuthPage from "./Component/AuthPage";
@@ -6,37 +5,29 @@ import JewelryCategory from "./Component/JewelryCategory";
 import HvacCategory from "./Component/HvacCategory";
 import ConsumerPreferenceLayout from "./Component/Reusables/ConsumerPreferenceLayout";
 import Category from "./Component/Reusables/Category";
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AuthPage/>}>
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<AuthPage />}>
+          <Route index exact element={<Home />} />
 
-            <Route index exact element={<Home/>}/>
-
-            {/* <Route path="jewelry_category">
-              <Route index exact element={<Category/>}/>
-              <Route path="consumer_preference" exact element={<ConsumerPreferenceLayout/>}/>
-            </Route>
-
-            <Route path="hvac_category">
-              <Route index exact element={<Category/>}/>
-              <Route path="consumer_preference" exact element={<ConsumerPreferenceLayout/>}/>
-            </Route> */}
-
-            <Route path="category">
-              <Route index exact element={<Category/>}/>
-              <Route path="consumer_preference" exact element={<ConsumerPreferenceLayout/>}/>
-            </Route>
-
-            <Route path="*" element={<h1>404 Not Found</h1>}/>
+          <Route path="category">
+            <Route index exact element={<Category />} />
+            <Route
+              path="consumer_preference"
+              exact
+              element={<ConsumerPreferenceLayout />}
+            />
           </Route>
 
-        </Routes>
-      </BrowserRouter>
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Component/Reusables/ConsumerPreferenceLayout.jsx
+++ src/Component/Reusables/ConsumerPreferenceLayout.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../../Services/axiosInstance";
+import toast from "react-hot-toast";
 
 const ConsumerPreferenceLayout = () => {
   const [loading, setLoading] = useState(false);
-
-  const handleDone = (e) => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  };
-
-  const [recommendedData, setRecommendedData] = useState([]);
   const [mainCriteriaPairs, setMainCriteriaPairs] = useState([]);
-  const [data, setData] = useState([""])
+  const [sliderValues, setSliderValues] = useState([]);
+  const [recommendedData, setRecommendedData] = useState([]);
+  const [data, setData] = useState([]);
+
+  const [apiRequest, setApiRequest] = useState({});
+  const [productComparison, setProductComparison] = useState([]);
 
   useEffect(() => {
     const get_attributes = async () => {
@@ -21,12 +18,11 @@ const ConsumerPreferenceLayout = () => {
         client_id: localStorage.getItem("client_id"),
         product_category_id: localStorage.getItem("product_category_id"),
       };
-      
 
       try {
         await axiosInstance.post("/get_attributes", getProduct).then((res) => {
-          console.log(res.data.data);
           setRecommendedData(res.data.data[0]);
+          setApiRequest(res.data.data[0]);
           setMainCriteriaPairs(res.data.data[0].main_criteria_pairs);
         });
       } catch (err) {
@@ -42,6 +38,113 @@ const ConsumerPreferenceLayout = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (mainCriteriaPairs.length > 0) {
+      const defaultValues = Array(mainCriteriaPairs.length).fill(5); // Assuming default value is 1
+      setSliderValues(defaultValues);
+    }
+  }, [mainCriteriaPairs]);
+
+  const convertSliderValue = (value) => {
+    switch (value) {
+      case 1:
+        return 1 / 5;
+      case 2:
+        return 1 / 4;
+      case 3:
+        return 1 / 3;
+      case 4:
+        return 1 / 2;
+      case 5:
+        return 1;
+      case 6:
+        return 2;
+      case 7:
+        return 3;
+      case 8:
+        return 4;
+      case 9:
+        return 5;
+      default:
+        return 1; // Default to 1 if value is out of range
+    }
+  };
+
+  const updateApiRequest = () => {
+    const updatedApiRequest = {
+      ...apiRequest,
+      user_importance: sliderValues.map(convertSliderValue),
+    };
+    console.log("Updated API Request:", updatedApiRequest); // Add this line
+    setApiRequest(updatedApiRequest);
+  };
+
+  const handleDone = async () => {
+    // updateApiRequest(); // Update apiRequest with current slider values
+
+    const updatedApiRequest = {
+      ...apiRequest,
+      user_importance: sliderValues.map(convertSliderValue),
+    };
+
+    console.log(updatedApiRequest);
+
+    try {
+      setLoading(true);
+
+      // Make API call with updated apiRequest
+      const response = await axiosInstance.post(
+        "/consumer_service",
+        updatedApiRequest
+      );
+      console.log(response.data);
+      if (response.data.error_code === 200) {
+        setData(response.data); // Set your data state here
+        setProductComparison(response.data.data.product_comparisons);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // const handleDone = async (e) => {
+  //   // Update apiRequest with current slider values
+  //   updateApiRequest();
+  //   const convertedValues = sliderValues.map((value) =>
+  //     convertSliderValue(value)
+  //   );
+  //   alert(convertedValues);
+
+  //   console.log(apiRequest)
+
+  //   try {
+  //     // Make API call with axios
+  //     const response = await axiosInstance.post("/consumer_service", apiRequest);
+
+  //     // Handle the response, for example, set the received data to the 'data' state
+
+  //     if(response.data.error_code === 200){
+  //       console.log(response.data);
+  //     }
+
+  //     setLoading(false);
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //     setLoading(false);
+  //   }
+  // };
+
+  const handleSliderChange = (index, event) => {
+    const newSliderValues = [...sliderValues];
+    newSliderValues[index] = parseInt(event.target.value);
+    setSliderValues(newSliderValues);
+    console.log("Slider Values Updated:", newSliderValues); // Add this line
+  };
+
   return (
     <>
       <section className="content-breadcrumps-below-content-height content-preference-section pt-1 overflow-scroll">
@@ -54,37 +157,48 @@ const ConsumerPreferenceLayout = () => {
                 <h4 className="title mb-1 pt-4 pb-3 px-4">
                   Consumer Preference
                 </h4>
-                <div className="range-bar-container slidecontainer  px-4 ">                  
-                  {mainCriteriaPairs &&
-                    mainCriteriaPairs.map((pair, index) => {
-                      return (
-                        <div
-                        className="d-flex my-4 align-items-center"
-                        key={index}
-                      >
-                        {/* <p className="p-2 mb-0 ">{index}</p> */}
-                        <p className="p-2 mb-0 w-25 sliderText">{pair[0]}</p>
-                        <div className="p-2 mb-0 flex-grow-1 ">
-                          <input
-                            type="range"
-                            className="form-control slider w-90"
-                            min="1"
-                            max="9"
-                            step="1"
-                            id={`slider-${index}`}
-                            defaultValue={5}
-                          />
-                        </div>
-                        <p className="p-2 mb-0 w-25 sliderText">{pair[1]}</p>
-                      </div>
-                      )
-                     
-                    })}
+                <div className="range-bar-container slidecontainer  px-4 ">
+                  <>
+                    {mainCriteriaPairs &&
+                      mainCriteriaPairs.map((pair, index) => {
+                        const key = `slider-${index}`; // Unique key for each slider
+                        return (
+                          <div
+                            className="d-flex my-4 align-items-center"
+                            key={key}
+                          >
+                            <p className="p-2 mb-0 w-25 sliderText">
+                              {pair[0]}
+                            </p>
+                            <div className="p-2 mb-0 flex-grow-1">
+                              <input
+                                type="range"
+                                className="form-control slider w-90"
+                                min="1"
+                                max="9"
+                                step="1"
+                                id={key}
+                                value={sliderValues[index] || 5} // Use default value if sliderValues[index] is falsy
+                                onChange={(event) =>
+                                  handleSliderChange(index, event)
+                                } // Event handler
+                              />
+                            </div>
+                            <p className="p-2 mb-0 w-25 sliderText">
+                              {pair[1]}
+                            </p>
+                          </div>
+                        );
+                      })}
+                  </>
                 </div>
-                
+
                 <div className="text-center px-4 my-2 mt-4">
-                  <div className="btn brand-color w-100" onClick={handleDone}>
-                    Done
+                  <div
+                    className="btn brand-color w-100 fw-bold"
+                    onClick={handleDone}
+                  >
+                    {loading ? "Loading, Please wait..." : "Done"}
                   </div>
                 </div>
               </div>
@@ -102,172 +216,64 @@ const ConsumerPreferenceLayout = () => {
                       alt="loading-gif"
                     />
                   </div>
-                ) : data.length ? (
+                ) : productComparison.length ? (
                   <div>
                     <h4 className="title mb-1 pt-4 mb-3 px-4">
                       Top Recommendations
                     </h4>
-                    <div className="top-recommendation-card-container  px-4">
-                    <div className="card mb-3 p-0 ">
-                        <div className="row g-0 ">
-                          <div className="col-md-12">
-                            <div className="card-body mb-0">
-                              <h5 className="card-title fw-bold">
-                                Radiant Rose Pendant
-                              </h5>
-                              <p className="card-text ">
-                                The pendant features a central rose motif,
-                                symbolizing love, beauty, and grace. The rose is
-                                intricately designed to showcase delicate
-                                petals, often with a lifelike texture and form.
-                              </p>
-                              <p className="card-text">
-                                <small className="card-text">
-                                  A pendant featuring a floral design with
-                                  diamond petals and a central diamond,
-                                  resembling the radiant rose style.
-                                </small>
-                              </p>
+                    <div className="top-recommendation-card-container px-4">
+                      {productComparison.map((item, index) => (
+                        <div className="card mb-3 p-0" key={index}>
+                          <div className="row g-0">
+                            <div className="col-md-12">
+                              <div className="card-body mb-0">
+                                <h5 className="card-title fw-bold">
+                                  {item.name}
+                                </h5>
+                                <p className="card-text">{item.desc}</p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-
-                        <div className="container-fluid  my-3 mb-4">
+                          <div className="container-fluid my-3 mb-4">
                           <div className="row mx-0">
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="$5200"
-                                  readOnly
-                                />
-                                <p className="special-label">Cost</p>
-                              </div>
-                            </div>
+  {/* Mapping non-quantified values into item.features */}
+  {item["non-quantified-values"].map((feature, index) => {
+    // Get the label and value from each feature object
+    const label = Object.keys(feature)[0]; // Extracting the label
+    const value = feature[label]; // Extracting the value
 
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="1.50"
-                                  readOnly
-                                />
-                                <p className="special-label">Carat</p>
-                              </div>
-                            </div>
+    // Calculate the width of the column dynamically based on the length of the label and value
+    const labelWidth = `${(label.length + 2) * 10}px`; // Adjust the multiplier according to your font size and padding
+    const valueWidth = `${(value.length + 2) * 10}px`; // Adjust the multiplier according to your font size and padding
+    const columnWidth = label.length > value.length ? labelWidth : valueWidth;
 
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="VS1"
-                                  readOnly
-                                />
-                                <p className="special-label">Clarity</p>
-                              </div>
-                            </div>
+    // Rendering JSX for each feature
+    return (
+      <div className="col mb-3 price-container" key={index} style={{ minWidth: columnWidth }}>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="w-100 pt-3 px-3 price-input-field pe-none"
+            value={value}
+            readOnly
+          />
+          <p className="special-label">{label}</p>
+        </div>
+      </div>
+    );
+  })}
+</div>
 
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="Round"
-                                  readOnly
-                                />
-                                <p className="special-label">Cut Brilliant</p>
-                              </div>
-                            </div>
                           </div>
                         </div>
-                      </div>
-
-                      <div className="card mb-3 p-0 ">
-                        <div className="row g-0 ">
-                          <div className="col-md-12">
-                            <div className="card-body mb-0">
-                              <h5 className="card-title fw-bold">
-                                Radiant Rose Pendant
-                              </h5>
-                              <p className="card-text ">
-                                The pendant features a central rose motif,
-                                symbolizing love, beauty, and grace. The rose is
-                                intricately designed to showcase delicate
-                                petals, often with a lifelike texture and form.
-                              </p>
-                              <p className="card-text">
-                                <small className="card-text">
-                                  A pendant featuring a floral design with
-                                  diamond petals and a central diamond,
-                                  resembling the radiant rose style.
-                                </small>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div className="container-fluid  my-3 mb-4">
-                          <div className="row mx-0">
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="$5200"
-                                  readOnly
-                                />
-                                <p className="special-label">Cost</p>
-                              </div>
-                            </div>
-
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="1.50"
-                                  readOnly
-                                />
-                                <p className="special-label">Carat</p>
-                              </div>
-                            </div>
-
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="VS1"
-                                  readOnly
-                                />
-                                <p className="special-label">Clarity</p>
-                              </div>
-                            </div>
-
-                            <div className="col mb-0   price-container">
-                              <div className="form-floating">
-                                <input
-                                  type="text"
-                                  className="w-100 pt-3 px-3 price-input-field pe-none"
-                                  value="Round"
-                                  readOnly
-                                />
-                                <p className="special-label">Cut Brilliant</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                      ))}
                     </div>
                   </div>
                 ) : (
                   <div className="d-flex justify-content-center align-items-center h-100">
                     <img
-                      src={require("../assets/loadings.gif")}
-                      className="w-15"
+                      src={require("../assets/NoDataFound.jpg")}
+                      className="w-75"
                       alt="loading-gif"
                     />
                   </div>
